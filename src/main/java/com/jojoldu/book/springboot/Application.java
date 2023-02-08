41d57cbcb837f39baae7ed727ad89b197c050b1c
iheee6-6 <iheee6@gmail.com>
2023-02-08 15:06:31 +0900

    네이버 로그인 연동 및 테스트코드 작성

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //jpa auditing 활성화
+//@EnableJpaAuditing //jpa auditing 활성화 ->Test때문에 주석처리하고 jpaConfig 파일 생성함
 @SpringBootApplication // 스프링 부트의 자동 설정, 스프링 bean 읽기와 생성을 모두 자동으로 설정함, 이것이 있는 위치부터 설정을 읽어가기때문에
 // 이 클래스는 가장 최상단에 있어야함.
 public class Application { //main 클래스
     public static void main(String[] args){
+        
         SpringApplication.run(Application.class, args);// 내장 was
     }
 }
--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -1,30 +1,38 @@
 package com.jojoldu.book.springboot.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.TestReporter;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
+
     @LocalServerPort
     private int port;
     @Autowired
@@ -32,12 +40,79 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    //MockMvc 사용하도록 개선
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before //테스트 시작전 mockMvc 인스턴스를 생성
+    public void setup(){
+        mvc= MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+    //
     @After
     public void tearDown() throws Exception{
         postsRepository.deleteAll();
     }
 
+    //forMock
     @Test
+    @WithMockUser(roles="USER") // 가짜 사용자를 만듬
+    public void Posts_등록된다() throws Exception{
+        //given
+        String title ="title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url ="http://localhost:"+port +"/api/v1/posts";
+
+        //when
+        mvc.perform(post(url) //perform : MockMvc를 통해 api를 테스트한다.
+                .contentType(MediaType.APPLICATION_JSON_UTF8) //body영역을 json으로 변환
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+        //then
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    @WithMockUser(roles="USER")
+    public void Posts_수정된다() throws Exception{
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        Long updateId = savedPosts.getId();
+        String excepctedTitle = "title2";
+        String expectedContent="content2";
+
+        PostsUpdateRequestDto requestDto=PostsUpdateRequestDto.builder().title(excepctedTitle).content(expectedContent).build();
+        String url ="http://localhost:"+port+"/api/v1/posts/"+updateId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+        //then
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(excepctedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+    //
+    /*
+    @Test
+    @WithMockUser(roles="USER") // 가짜 사용자를 만듬
     public void Posts_등록된다() throws Exception{
         //given
         String title ="title";
@@ -64,6 +139,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception{
         //given
         Posts savedPosts = postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
@@ -85,5 +161,5 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(excepctedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-    }
+    }*/
 }
